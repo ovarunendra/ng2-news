e6a7d0280d6d6e525272e2ee7ee82c688d8edbcf
Varunendra Pratap Singh <vpsingh016@gmail.com>
2016-09-29 12:16:19 +0530

    performance improvment by changing api

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input,  OnInit } from '@angular/core';
 
-import { HackerNewsAPIService } from '../hackernews-api.service';
 
 @Component({
   selector: 'app-item',
@@ -9,15 +8,11 @@ import { HackerNewsAPIService } from '../hackernews-api.service';
 })
 export class ItemComponent implements OnInit {
 
-  @Input() itemID: number;
-  item;
+  @Input() item;
 
-  constructor(private _hackerNewsAPIService: HackerNewsAPIService) { }
+  constructor() { }
 
   ngOnInit() {
-    this._hackerNewsAPIService.fetchItem(this.itemID).subscribe(data => {
-      this.item = data;
-    }, error => console.log('Could not load item' + this.itemID));
   }
 
 }
